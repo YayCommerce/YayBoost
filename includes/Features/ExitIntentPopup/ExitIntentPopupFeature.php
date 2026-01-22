347788b3b4ad21cf2487eca10176df0328e51775
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2026-01-22 15:24:37 +0700

    Fix feedback

--- includes/Features/ExitIntentPopup/ExitIntentPopupFeature.php
+++ includes/Features/ExitIntentPopup/ExitIntentPopupFeature.php
@@ -16,99 +16,99 @@ use YayBoost\Features\AbstractFeature;
  */
 class ExitIntentPopupFeature extends AbstractFeature {
 
-	/**
-	 * Feature ID
-	 *
-	 * @var string
-	 */
-	protected $id = 'exit_intent_popup';
+    /**
+     * Feature ID
+     *
+     * @var string
+     */
+    protected $id = 'exit_intent_popup';
 
-	/**
-	 * Feature name
-	 *
-	 * @var string
-	 */
-	protected $name = 'Exit-Intent Popup';
+    /**
+     * Feature name
+     *
+     * @var string
+     */
+    protected $name = 'Exit-Intent Popup';
 
-	/**
-	 * Feature description
-	 *
-	 * @var string
-	 */
-	protected $description = 'Show a popup when customers try to leave with items in cart.<br/> Offer discount to complete purchase NOW.';
+    /**
+     * Feature description
+     *
+     * @var string
+     */
+    protected $description = 'Show a popup when customers try to leave with items in cart.<br/> Offer discount to complete purchase NOW.';
 
-	/**
-	 * Feature category
-	 *
-	 * @var string
-	 */
-	protected $category = 'others';
+    /**
+     * Feature category
+     *
+     * @var string
+     */
+    protected $category = 'others';
 
-	/**
-	 * Feature icon (Phosphor icon name)
-	 *
-	 * @var string
-	 */
-	protected $icon = 'arrow-square-out';
+    /**
+     * Feature icon (Phosphor icon name)
+     *
+     * @var string
+     */
+    protected $icon = 'arrow-square-out';
 
-	/**
-	 * Display priority
-	 *
-	 * @var int
-	 */
-	protected $priority = 2;
+    /**
+     * Display priority
+     *
+     * @var int
+     */
+    protected $priority = 2;
 
-	/**
-	 * Constructor
-	 *
-	 * @param \YayBoost\Container\Container $container DI container.
-	 */
-	public function __construct( $container ) {
-		parent::__construct( $container );
-	}
+    /**
+     * Constructor
+     *
+     * @param \YayBoost\Container\Container $container DI container.
+     */
+    public function __construct( $container ) {
+        parent::__construct( $container );
+    }
 
-	/**
-	 * Initialize the feature
-	 *
-	 * @return void
-	 */
-	public function init(): void {
-		if ( ! $this->is_enabled() ) {
-			return;
-		}
+    /**
+     * Initialize the feature
+     *
+     * @return void
+     */
+    public function init(): void {
+        if ( ! $this->is_enabled() ) {
+            return;
+        }
 
-		// // Initialize block
-		// new LiveVisitorCountBlock( $this );
-	}
+        // // Initialize block
+        // new LiveVisitorCountBlock( $this );
+    }
 
-	/**
-	 * Get default settings
-	 *
-	 * @return array Default settings.
-	 */
-	protected function get_default_settings(): array {
-		return array_merge(
-			parent::get_default_settings(),
-			array(
-				'enabled'  => true,
-				'trigger'  => array(
-					'leaves_viewport'     => true,
-					'back_button_pressed' => true,
-				),
-				'offer'    => array(
-					'type'    => 'percent',
-					'value'   => 20,
-					'prefix'  => 'GO-',
-					'expires' => 1, // 1 hour
-				),
+    /**
+     * Get default settings
+     *
+     * @return array Default settings.
+     */
+    protected function get_default_settings(): array {
+        return array_merge(
+            parent::get_default_settings(),
+            [
+                'enabled'  => true,
+                'trigger'  => [
+                    'leaves_viewport'     => true,
+                    'back_button_pressed' => true,
+                ],
+                'offer'    => [
+                    'type'    => 'percent',
+                    'value'   => 20,
+                    'prefix'  => 'GO-',
+                    'expires' => 1,
+                ],
 
-				'content'  => array(
-					'headline'    => __( 'Wait! Don\'t leave!', 'yayboost' ),
-					'message'     => __( 'Completed your order now and receive 10% discount', 'yayboost' ),
-					'button_text' => __( 'Complete my order', 'yayboost' ),
-				),
-				'behavior' => 'checkout_page',
-			)
-		);
-	}
+                'content'  => [
+                    'headline'    => __( 'You\'re leaving?', 'yayboost' ),
+                    'message'     => __( 'But we have a discount coupon waiting for you', 'yayboost' ),
+                    'button_text' => __( 'Get 20% discount', 'yayboost' ),
+                ],
+                'behavior' => 'checkout_page',
+            ]
+        );
+    }
 }
